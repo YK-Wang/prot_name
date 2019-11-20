856c8b3259e729687d14b4dbb356c5c21cf63f13
YK-Wang <yankunwang1988@126.com>
2019-11-20 09:14:46 +0800

    Add files via upload

---
+++ prot_name.pl
@@ -0,0 +1,34 @@
+#!usr/bin/perl -w
+use strict;
+
+my $file_blast = "file_location\\prot_individual_code.orient.stripped.trim.unique.pick.fasta";
+open (FIN,$file_blast)||die"Where's the blast file ?";
+open (FOUT,">>file_location\\prot_individual_code.orient.stripped.trim.unique.pick.fasta.accnos");
+
+my %seq;
+my $key;
+while(<FIN>){
+	if ($_ =~ /^>(.*)/) {
+		$key = $1;
+		$seq{$key} = "";
+	}
+	else {
+		$seq{$key} .= $_;
+	}
+}
+#print Dumper %seq;
+
+foreach (keys %seq) {
+	if ( $seq{$_} =~ /\*/ ){
+		next;
+	}
+	else {
+	print FOUT (">$_\n");
+	}
+}
+
+close FIN;
+close FOUT;
+
+print "\a\a\aFinish!\a\n       \\   |   /\n         ____ \n  ---   /     \\   ---\n       | ~   ~ |\n  ---   \\__O__/   --- \n        /     \\   \n";
+
